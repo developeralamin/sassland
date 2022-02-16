2d6785f955c11988b913d1686fe5cab228a9d142
developeralamin <ribinaofficial018@gmail.com>
2022-02-17 01:51:32 +0600

    Navbar update now

--- vendors/js/custom.js
+++ vendors/js/custom.js
@@ -1,55 +1,30 @@
-// $(document).ready(function(){
-// menu area
-// menu area
-// 	 $('.close').click(function(){
-//       $('.sidebar_menu').hide();
+$(document).ready(function () {
+  /*Sticky menu*/
 
-//    });
+$(window).on('scroll', function(){
+    if($(this).scrollTop() >10){
+        $('.header_top').addClass('sticky_nav');
+    }else{
+        $('.header_top').removeClass('sticky_nav');
+  }
 
-// 	$('.close').on('click', function (e) {
-//        $('.sidebar_menu').toggle();
-
-//       });
-
-// 		 // scroll-bar
-
-//          $('.scroll-bar a').click(function(){
-// 			    $('html,body').animate({
-// 					scrollTop:0
-// 				},2000);
-// 				return false;
-// 		 });
-
-// 		 $(window).scroll(function(){
-// 			 if($(window).scrollTop()>900){
-// 				  $('.scroll-bar a').fadeIn(2000);
-// 			 }else{
-// 				  $('.scroll-bar a').fadeOut(2000);
-// 			 }
-// 		 });
-// });
-
-const cartPopup = document.getElementById("cartPopup");
-const clickCartBtn = document.getElementById("cartIcon");
-const closePopup = document.getElementById("closePopup");
-
-clickCartBtn.addEventListener("click", function () {
-  cartPopup.classList.add("activeCartPopup");
-});
-
-closePopup.addEventListener("click", function () {
-  cartPopup.classList.remove("activeCartPopup");
 });
 
 
-const menuPopup = document.getElementById("menuPopup");
-const mobile_toggle = document.getElementById("mobile_toggle");
-const closePopupMenu = document.getElementById("closePopupMenu");
+  /* Mobile menu */
+$('.mobile_menu .open_nav').click(function(){
+    $('nav').slideDown(1000);
+    $(this).fadeOut();
+    $('.close_nav').fadeIn();
+});
 
-mobile_toggle.addEventListener('click', function () {
-   menuPopup.classList.add("activeMenuPopup");
-})
+$('.mobile_menu .close_nav').click(function(){
+    $('nav').slideUp(1000);
+    $(this).fadeOut();
+    $('.open_nav').fadeIn();
+});
 
-closePopupMenu.addEventListener('click', function () {
-     menuPopup.classList.remove("activeMenuPopup");
+
+
+
 });
